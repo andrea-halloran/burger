9a75e39a1f81edc4cdaab9e667f9a6631dbb9ea2
ahalloran <ahalloran01@gmail.com>
2018-11-09 22:30:23 -0600

    in case this breaks

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,7 @@ var router = express.Router();
 // import burger.js 
 var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
+
 router.get("/", function (req, res) {
     burger.all(function (data) {
         var hbsObject = {
@@ -23,7 +23,7 @@ router.post("/api/burgers", function (req, res) {
     ], [
         req.body.name, req.body.devour
     ], function (result) {
-        // Send back the ID of the new quote
+       
         res.json({
             id: result.insertId
         });
@@ -39,7 +39,7 @@ router.put("/api/burgers/:id", function (req, res) {
         devour: req.body.devour
     }, condition, function (result) {
         if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
+            
             return res.status(404).end();
         } else {
             res.status(200).end();
@@ -47,5 +47,4 @@ router.put("/api/burgers/:id", function (req, res) {
     });
 });
 
-// export the router at the end of the file 
 module.exports = router;
